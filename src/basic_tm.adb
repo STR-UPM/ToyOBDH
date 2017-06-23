a121a2fc6218a781b4c8295bf7875e5e1619fd33
Juan A. de la Puente <jpuente@dit.upm.es>
2017-06-23 17:33:12 +0200

    Update copyright notice on file

--- src/basic_tm.adb
+++ src/basic_tm.adb
@@ -1,11 +1,33 @@
--- $Id: basic_tm_task.adb 80 2017-04-27 13:05:18Z jpuente $
--- Project OBDH
--- Basic TM task body
--- Copyright (c) 2017 Juan Antonio de la Puente <jpuente@dit.upm.es>
--- Permission to copy and modify are granted under the terms of
--- the GNU General Public License (GPL).
--- See http://www.gnu.org/licenses/licenses.html#GPL for the details
---------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+--                                                                          --
+--          Copyright (C) 2017, Universidad Politécnica de Madrid           --
+--                                                                          --
+--  Redistribution and use in source and binary forms, with or without      --
+--  modification, are permitted provided that the following conditions are  --
+--  met:                                                                    --
+--     1. Redistributions of source code must retain the above copyright    --
+--        notice, this list of conditions and the following disclaimer.     --
+--     2. Redistributions in binary form must reproduce the above copyright --
+--        notice, this list of conditions and the following disclaimer in   --
+--        the documentation and/or other materials provided with the        --
+--        distribution.                                                     --
+--     3. Neither the name of the copyright holder nor the names of its     --
+--        contributors may be used to endorse or promote products derived   --
+--        from this software without specific prior written permission.     --
+--                                                                          --
+--   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
+--   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
+--   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
+--   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
+--   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
+--   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
+--   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
+--   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
+--   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
+--   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+--                                                                          --
+------------------------------------------------------------------------------
 with Measurements;       use Measurements;
 with TM;                 use TM;
 with Buffer;
